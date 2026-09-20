31238d30b37917c8a2b4d08b1c8bb093d4209d69
Sayan the researcher <sm6881164@gmail.com>
2026-09-20 15:17:00 +0530

    test: add direct reference-counting stress harness

---
+++ selfhost/rc_runtime_stress.c
@@ -0,0 +1,9 @@
+#include "rc_runtime.h"
+int main(void){
+  char *s=sx_rc_new(5); memcpy(s,"start",5);
+  for(int i=0;i<2000;i++){ char *n=sx_concat(s,"x"); sx_rc_release(s); s=n; }
+  printf("%zu\n",sx_rc_from(s)->size);
+  puts("gc-rc-ok");
+  sx_rc_release(s);
+  return 0;
+}
