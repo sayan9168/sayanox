f14558b00dd78197d093d97ec70eef68e94b2d04
Sayan the researcher <sm6881164@gmail.com>
2026-09-20 15:17:27 +0530

    test: exercise retain and release in RC stress harness

--- selfhost/rc_runtime_stress.c
+++ selfhost/rc_runtime_stress.c
@@ -1,6 +1,6 @@
 #include "rc_runtime.h"
 int main(void){
-  char *s=sx_rc_new(5); memcpy(s,"start",5);
+  char *s=sx_rc_new(5); memcpy(s,"start",5); sx_rc_retain(s); sx_rc_release(s);
   for(int i=0;i<2000;i++){ char *n=sx_concat(s,"x"); sx_rc_release(s); s=n; }
   printf("%zu\n",sx_rc_from(s)->size);
   puts("gc-rc-ok");
